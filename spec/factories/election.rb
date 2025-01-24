e2b8c15bec567d33603f26c5170dadf2396dbfb6
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-01-24 16:58:09 -0300

    fix: shoudamatters

---
+++ spec/factories/election.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :election do
+    title { "Election 2025" }
+    description { "This is the description for Election 2025" }
+    status { :draft } 
+    start_time { 1.day.from_now }
+    end_time { 2.days.from_now }
+    election_day { Time.zone.today + 1.week }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,7 +67,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
+  config.include FactoryBot::Syntax::Methods
 
   # shoulda matchers config
   Shoulda::Matchers.configure do |config| 
