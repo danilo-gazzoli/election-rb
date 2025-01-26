130d130a18ce8502f2e46b06a2f5b42cc486f7e5
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-01-26 11:16:33 -0300

    feat: rails generate controller Elections index show new create edit update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'elections/index'
+  get 'elections/show'
+  get 'elections/new'
+  get 'elections/create'
+  get 'elections/edit'
+  get 'elections/update'
+  get 'elections/destroy'
   get "up" => "rails/health#show", as: :rails_health_check
 end
