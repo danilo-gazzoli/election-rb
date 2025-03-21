6ee69e19a3f245b03f7e6df081ed146a90c954b7
Danilo Martins Gazzoli <dmartinsgazzoli@gmail.com>
2025-03-21 15:11:21 -0300

    feat(candidate): run rails generate controller
    Co-authored-by: Matozo0 <filipaomat100@gmail.com>.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'candidates/index'
+  get 'candidates/show'
+  get 'candidates/new'
+  get 'candidates/create'
+  get 'candidates/edit'
+  get 'candidates/update'
+  get 'candidates/destroy'
   resources :elections
   resources :offices
   resources :parties
