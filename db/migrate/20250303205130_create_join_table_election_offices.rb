b5072c33050aa79e597d09bd5081dae14d998819
danilo-gazzoli <dmartinsgazzoli@gmail.com>
2025-03-28 22:43:22 -0300

    fix(election-offices migration): the duplication of create table election-offices migration are fixed

--- db/migrate/20250303205130_create_join_table_election_offices.rb
+++ db/migrate/20250303205130_create_join_table_election_offices.rb
@@ -3,7 +3,6 @@
 class CreateJoinTableElectionOffices < ActiveRecord::Migration[7.1]
   def change
     create_join_table :elections, :offices do |t|
-      t.index %i[election_id office_id]
       t.index %i[office_id election_id]
     end
 
